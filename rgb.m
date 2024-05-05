57f6d5718988a9189a5d22f4b4e0830a25e1a785
R210433 <159885972+R210433@users.noreply.github.com>
2024-05-06 03:29:43 +0530

    Add files via upload

---
+++ rgb.m
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Read the image
+m = plt.imread("/home/rguktrkvalley/Pictures/recent_addiction.jpg")
+
+# Split channels
+red = m[:, :, 0]
+green = m[:, :, 1]
+blue = m[:, :, 2]
+
+# Save each channel to a CSV file
+np.savetxt("red.csv", red.flatten(), delimiter=",")
+np.savetxt("green.csv", green.flatten(), delimiter=",")
+np.savetxt("blue.csv", blue.flatten(), delimiter=",")
