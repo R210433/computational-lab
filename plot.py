e0cbd47340d3ebe736d57928771e9c54eac9fe50
R210433 <159885972+R210433@users.noreply.github.com>
2024-05-06 02:35:34 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,14 @@
+import numpy as np
+from matplotlib import pyplot as plt
+t=np.arange(0,1,0.01)
+f=5
+x=np.sin(2*np.pi*f*t)
+plt.plot(t,x)
+plt.xlabel('time')
+plt.ylabel('amplitute')
+plt.title('sine wave')
+plt.show()
+
+
+
+
