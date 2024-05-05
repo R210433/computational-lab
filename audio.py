5606415b0b5bcc9a64f5289ae2b594cda7cd705c
R210433 <159885972+R210433@users.noreply.github.com>
2024-05-06 03:42:11 +0530

    audio.py

--- audio.py
+++ audio.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
-file_path = '/home/rguktrkvalley/python(Cl)/Chorus.wav'
-sample, data_audio = wavfile.read(file_path)
-timing= np.arange(len(data_audio)) / sample
-plt.plot(timing,data_audio)
+path = '/home/rguktrkvalley/python(Cl)/Chorus.wav'
+sample, audio_data = wavfile.read(file_path)
+time= np.arange(len(audio_data)) / sample
+plt.plot(time,audio_data)
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
 plt.title('Audio Signal')
